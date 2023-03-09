467add39742c6259ec891db57a0789a6d2915214
graphinfinite <graphinfinit@gmail.com>
2023-03-09 07:49:32 +0300

    logiform

---
+++ front/deepflower/src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+      <form class="login-form" @submit.prevent="login">
+        <h2>Sign in</h2>
+        <label>Username </label>
+        <input required v-model="username" type="text" placeholder="..." />
+        <label> Password </label>
+        <input
+          required
+          v-model="password"
+          type="password"
+          placeholder="..."
+        />
+        <hr />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <style>
+  .login {
+    display: flex;
+    flex-direction: row;
+    width: 200px;
+    padding: 10px;
+  }
+  </style>
+  
+  <script>
+  export default {
+    name: "login-form",
+    data() {
+      return {
+        username: "",
+        password: ""
+      };
+    },
+    methods: {
+      login: function() {
+        const { username, password } = this;
+        this.$store.dispatch("/auth/login", { username, password }).then(() => {
+          this.$router.push("/");
+        });
+      }
+    }
+  };
+  </script>
--- front/deepflower/src/main.js
+++ front/deepflower/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+// import Axios from 'axios'
+
+// App.prototype.$http = Axios;
+// const token = localStorage.getItem('token')
+// if (token) {
+//   App.prototype.$http.defaults.headers.common['Authorization'] = token
+// }
+
 createApp(App).mount('#app')
